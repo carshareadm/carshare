8baf10510bb1491f76900ce3ffc7b9c6d40d2960
adrianchen91 <s3584721@student.rmit.edu.au>
2018-03-20 20:09:13 +1100

    Missed newline >.>

--- client/modules/Layout/components/Footer/Footer.js
+++ client/modules/Layout/components/Footer/Footer.js
@@ -7,9 +7,14 @@ import styles from './Footer.css';
 export function Footer() {
   return (
     <footer className={styles.footer}>
-    		<Link to="/">Contact Us</Link> &copy; 2018 RMIT PP1 Group 3
+      <Link to="/home">www.shacar.com.au</Link><br />
+    	<Link to="/termsandconditions">Terms and Conditions of use</Link>
     </footer>
   );
 }
+
+Footer.contextTypes = {
+  router: React.PropTypes.object,
+};
 
 export default Footer;
