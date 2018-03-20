92d626a631a87f1c085a65b9a7906c0569b28171
adrianchen91 <s3584721@student.rmit.edu.au>
2018-03-20 20:19:01 +1100

    Added comment for future reference - Footer.js

--- client/modules/Layout/components/Footer/Footer.js
+++ client/modules/Layout/components/Footer/Footer.js
@@ -15,6 +15,14 @@ export function Footer() {
 
 Footer.contextTypes = {
   router: React.PropTypes.object,
+  /* 
+  To satisfy layout.js requirement
+  See below
+  "App.propTypes = {
+    children: PropTypes.object.isRequired,
+    dispatch: PropTypes.func.isRequired,
+  };""
+  */
 };
 
 export default Footer;
