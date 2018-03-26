6dc7066b94fa3681c5881e0726914b360a8c69a5
adrianchen91 <s3584721@student.rmit.edu.au>
2018-03-26 12:08:05 +1100

    Cleaned up redudant error handling. Rectified error in location 3 check - checked for Melbourne in creation of Perth

--- server/dummyData.js
+++ server/dummyData.js
@@ -26,315 +26,327 @@ module.exports = function() {
     if (count < 1) {
       console.log('Adding users...');
 
-      let userOne = new User();
-      userOne.email = "carshareadm@gmail.com";
-      userOne.mobile = "0412345678";
-      userOne.password = "shacar";
-      userOne.isAdmin = true;
-      userOne.save({}).catch(function(err) {
+      let user1 = new User();
+      user1.email = "carshareadm@gmail.com";
+      user1.mobile = "0412345678";
+      user1.password = "shacar";
+      user1.isAdmin = true;
+      user1.save({}).catch(function(err) {
         if (11000 === err.code || 11001 === err.code) {
-          console.log("DB Error: Duplicate of user:", userOne.email);
+          console.log("DB Error: Duplicate of user:", user1.email);
         }
       });
     
       //TestUsers
     
-      let userTwo = new User();
-      userTwo.email = "user1@test.com";
-      userTwo.mobile = "0411111111";
-      userTwo.password = "user1";
+      let user2 = new User();
+      user2.email = "user1@test.com";
+      user2.mobile = "0411111111";
+      user2.password = "user1";
     
-      //UserTwo license block
-      let licenseU1 = new License();
-      licenseU1.licenseNumber = "ABC123456";
+      //User2 license block
+      let licenseU2 = new License();
+      licenseU2.licenseNumber = "ABC123456";
     
-      let imagelicenseU1 = new Image();
-      //userTwo image block
-      imagelicenseU1.fileHandle = "userTwo";
-      imagelicenseU1.extension = "jpg";
-      imagelicenseU1.save({}).catch(function(err) {
+      let imagelicenseU2 = new Image();
+      //user2 image block
+      imagelicenseU2.fileHandle = "user2";
+      imagelicenseU2.extension = "jpg";
+      imagelicenseU2.save({}).catch(function(err) {
         if (11000 === err.code || 11001 === err.code) {
-          console.log("DB Error: Duplicate of license:", imagelicenseU1.fileHandle);
+          console.log("DB Error: Duplicate of license:", imagelicenseU2.fileHandle);
         }
       });
-      licenseU1.image = imagelicenseU1;
-      licenseU1.save({}).catch();
-      userTwo.license = licenseU1;
+      licenseU2.image = imagelicenseU2;
+      licenseU2.save({}).catch();
+      user2.license = licenseU2;
     
-      let addressU1 = new Address();
-      //userTwo address block
-      addressU1.street1 = "1 Dickson place";
-      addressU1.street2 = "";
-      (addressU1.suburb = "Dickson"), (addressU1.state = "ACT");
-      addressU1.postCode = "2602";
-      addressU1.save({}).catch();
-      userTwo.address = addressU1;
+      let addressU2 = new Address();
+      //user2 address block
+      addressU2.street1 = "1 Dickson place";
+      addressU2.street2 = "";
+      (addressU2.suburb = "Dickson"), (addressU2.state = "ACT");
+      addressU2.postCode = "2602";
+      addressU2.save({}).catch();
+      user2.address = addressU2;
     
-      let creditCardU1 = new CreditCard();
-      //userTwo address block
-      creditCardU1.cardNumber = " 3778 123456 12345";
-      creditCardU1.nameOnCard = "userTwo";
-      creditCardU1.ccv = "3547";
-      creditCardU1.expiryMonth = 12;
-      creditCardU1.expiryYear = 2020;
-      creditCardU1.save({}).catch();
-      userTwo.creditCard = creditCardU1;
+      let creditCardU2 = new CreditCard();
+      //user2 address block
+      creditCardU2.cardNumber = " 3778 123456 12345";
+      creditCardU2.nameOnCard = "user2";
+      creditCardU2.ccv = "3547";
+      creditCardU2.expiryMonth = 12;
+      creditCardU2.expiryYear = 2020;
+      creditCardU2.save({}).catch();
+      user2.creditCard = creditCardU2;
     
-      let confirmationCodeU1 = new ConfirmationCode();
-      confirmationCodeU1.code = "ABC123456789";
-      confirmationCodeU1.user = userTwo;
-      confirmationCodeU1.expiresAt = "2018-05-01";
-      confirmationCodeU1.save({}).catch();
-      userTwo.confirmationCodes = confirmationCodeU1;
+      let confirmationCodeU2 = new ConfirmationCode();
+      confirmationCodeU2.code = "ABC123456789";
+      confirmationCodeU2.user = user2;
+      confirmationCodeU2.expiresAt = "2018-05-01";
+      confirmationCodeU2.save({}).catch();
+      user2.confirmationCodes = confirmationCodeU2;
     
-      userTwo.save({}).catch(function(err) {
+      user2.save({}).catch(function(err) {
         if (11000 === err.code || 11001 === err.code) {
-          console.log("DB Error: Duplicate of user:", userTwo.email);
+          console.log("DB Error: Duplicate of user:", user2.email);
         }
       });
     }
   });
   
   // add locations if there are none already in the system
-  let locationOne = new Location();
-  let coordinateOne = new Coordinate();
-  Location.findOne({'name': "Sydney Airport"})
+  let location1 = new Location();
+
+  var nameLocation1 = "Sydney Airport";
+  Location.findOne({'name': nameLocation1})
           .populate('coordinates')
           .exec((error, location) => {
             if (!location) {
-              console.log('Adding Sydney Airport...');
-              coordinateOne.latitude = "-33.947346";
-              coordinateOne.longitude = "151.179428";
-              coordinateOne.save({}).catch();
-              locationOne.name = "Sydney Airport";
-              locationOne.coordinates = coordinateOne;
-              locationOne.save({}).catch();
+              let coordinate1 = new Coordinate();
+              /* coordinate1 only used within location1 creation
+              no need to be set if location is already found
+              */
+              console.log('Adding', nameLocation1, '...');
+              coordinate1.latitude = "-33.947346";
+              coordinate1.longitude = "151.179428";
+              coordinate1.save({}).catch();
+              location1.name = nameLocation1;
+              location1.coordinates = coordinate1;
+              location1.save({}).catch();
             }
             else {
-              console.log('Sydney Airport already exists');
-              locationOne = location;
+              console.log("DB Error: Location \'", nameLocation1, "\' already exists.");
+              location1 = location;
             }
           });
   
-  let locationTwo = new Location();
-  let coordinateTwo = new Coordinate();
-  Location.findOne({'name': "Melbourne Airport"})
+  let location2 = new Location();
+  
+  var nameLocation2 = "Melbourne Airport";
+  Location.findOne({'name': nameLocation2})
           .populate('coordinates')
           .exec((error, location) => {
             if (!location) {
-              console.log('Adding Melbourne Airport...');
-              coordinateTwo.latitude = "-37.669012";
-              coordinateTwo.longitude = "144.841027";
-              coordinateTwo.save({}).catch();
-              locationTwo.name = "Melbourne Airport";
-              locationTwo.coordinates = coordinateTwo;
-              locationTwo.save({}).catch();
+              let coordinate2 = new Coordinate();
+               /* coordinate2 only used within location2 creation
+              no need to be set if location is already found
+              */
+              console.log('Adding', nameLocation2, '...');
+              coordinate2.latitude = "-37.669012";
+              coordinate2.longitude = "144.841027";
+              coordinate2.save({}).catch();
+              location2.name = nameLocation2;
+              location2.coordinates = coordinate2;
+              location2.save({}).catch();
             }
             else {
-              console.log('Melbourne Airport already exists');
-              locationTwo = location;
+              console.log("DB Error: Location \'", nameLocation2, "\' already exists.");
+              location2 = location;
             }
           });
 
-  let locationThree = new Location();
-  let coordinateThree = new Coordinate();
-  Location.findOne({'name': "Melbourne Airport"})
+  let location3 = new Location();
+
+  var nameLocation3 = "Perth Airport";
+  Location.findOne({'name': nameLocation3})
   .populate('coordinates')
   .exec((error, location) => {
     if (!location) {
-      console.log('Adding Perth Airport...');
-      coordinateThree.latitude = "-31.953512";
-      coordinateThree.longitude = "115.857048";
-      coordinateThree.save({}).catch();
-      locationThree.name = "Perth Airport";
-      locationThree.coordinates = coordinateThree;
-      locationThree.save({}).catch();
+      let coordinate3 = new Coordinate();
+      /* coordinate2 only used within location2 creation
+      no need to be set if location is already found
+      */
+     console.log('Adding', nameLocation3, '...');
+      coordinate3.latitude = "-31.953512";
+      coordinate3.longitude = "115.857048";
+      coordinate3.save({}).catch();
+      location3.name = nameLocation3;
+      location3.coordinates = coordinate3;
+      location3.save({}).catch();
     }
     else {
-      console.log('Perth Airport already exists');
-      locationThree = location;
+      console.log("DB Error: Location \'", nameLocation3, "\' already exists.");
+      location3 = location;
     }
   }); 
 
   // add vehicle types if not already in the system
   let typeSmall = new VehicleType();
-  VehicleType.findOne({'name': 'small'}, (error, vehicleType) => {
+  var nameCarType1 = "small";
+  VehicleType.findOne({'name': nameCarType1}, (error, vehicleType) => {
     if(!vehicleType) {
-      typeSmall.name = "small";
+      typeSmall.name = nameCarType1;
       typeSmall.hourlyRate = 7;
-      typeSmall.save({}).catch(function(err) {
-        if (11000 === err.code || 11001 === err.code) {
-          console.log("DB Error: Duplicate of type:", typeSmall.name);
-        }
-      });   
+      typeSmall.save({}).catch();   
     }
     else {
-      console.log('small vehicleType already exists')
+      console.log("DB Error: vehicleType \'", nameCarType1, "\' already exists.");
       typeSmall = vehicleType;
     }
   });
 
 
   let typeSports = new VehicleType();
-  VehicleType.findOne({'name': 'sports'}, (error, vehicleType) => {
+  var nameCarType2 = "sports";
+  VehicleType.findOne({'name': nameCarType2}, (error, vehicleType) => {
     if(!vehicleType) {
-      typeSports.name = "sports";
+      typeSports.name = nameCarType2;
       typeSports.hourlyRate = 8.75;
-      typeSports.save({}).catch(function(err) {
-        if (11000 === err.code || 11001 === err.code) {
-          console.log("DB Error: Duplicate of type:", typeSports.name);
-        }
-      });      
+      typeSports.save({}).catch();      
     }
     else {
-      console.log('sports vehicleType already exists')
+      console.log("DB Error: vehicleType \'", nameCarType2, "\' already exists.");
       typeSports = vehicleType;
     }
   });
 
   let typeLuxury = new VehicleType();
-  VehicleType.findOne({'name': 'luxury'}, (error, vehicleType) => {
+  var nameCarType3 = "luxury";
+  VehicleType.findOne({'name': nameCarType3}, (error, vehicleType) => {
     if(!vehicleType) {
-      typeLuxury.name = "luxury";
+      typeLuxury.name = nameCarType3;
       typeLuxury.hourlyRate = 10.5;
-      typeLuxury.save({}).catch(function(err) {
-        if (11000 === err.code || 11001 === err.code) {
-          console.log("DB Error: Duplicate of type:", typeLuxury.name);
-        }
-      });
+      typeLuxury.save({}).catch();
     }
     else {
-      console.log('luxury vehicleType already exists')
-      typeLuxury = vehicleType;
+      console.log("DB Error: vehicleType \'", nameCarType3, "\' already exists.");
+      typeLuxury = vehicleType; //Links variable to existing type
     }
   });
 
 
   let typeSuv = new VehicleType();
-  VehicleType.findOne({'name': 'suv'}, (error, vehicleType) => {
+  var nameCarType4 = "suv";
+  VehicleType.findOne({'name': nameCarType4}, (error, vehicleType) => {
     if(!vehicleType) {
-      typeSuv.name = "suv";
+      typeSuv.name = nameCarType4;
       typeSuv.hourlyRate = 10.5;
-      typeSuv.save({}).catch(function(err) {
-        if (11000 === err.code || 11001 === err.code) {
-          console.log("DB Error: Duplicate of type:", typeSuv.name);
-        }
-      });
+      typeSuv.save({}).catch();
     }
     else {
-      console.log('suv vehicleType already exists')
+      console.log("DB Error: vehicleType \'", nameCarType4, "\' already exists.");
       typeSuv = vehicleType;
     }
   });
 
 
   // add cars if there are none already in the system
-  Car.findOne({'rego': 'ABC123'}, (error, car) => {
+  var regoC1 = 'ABC123';
+  Car.findOne({'rego': regoC1}, (error, car) => {
     if(!car) {
-      let carOne = new Car();
-      (carOne.rego = "ABC123"), (carOne.make = "Hyundai");
-      carOne.model = "Getz";
-      carOne.colour = "white";
-      carOne.year = "2017";
-      carOne.seats = "5";
-      carOne.doors = "3";
-      carOne.vehicleType = typeSmall;
-      carOne.location = locationOne;
-      carOne.movements = new Movement();
-      carOne.movements.car = carOne;
+      let car1 = new Car();
+      (car1.rego = regoC1), (car1.make = "Hyundai");
+      car1.model = "Getz";
+      car1.colour = "white";
+      car1.year = "2017";
+      car1.seats = "5";
+      car1.doors = "3";
+      car1.vehicleType = typeSmall;
+      car1.location = location1;
+      car1.movements = new Movement();
+      car1.movements.car = car1;
       //Movement Block
       let movementsC1 = new Movement();
-      movementsC1.car = carOne;
-      movementsC1.coordinates = locationOne.coordinates;
+      movementsC1.car = car1;
+      movementsC1.coordinates = location1.coordinates;
       movementsC1.save({}).catch();
     
-      carOne.movements = movementsC1;
-      carOne.save({}).catch(function(err) {
-        if (11000 === err.code || 11001 === err.code) {
-          console.log("DB Error: Duplicate of car:", carOne.rego);
-        }
-      });
+      car1.movements = movementsC1;
+      car1.save({}).catch();
+    }
+    else
+    {
+      console.log("DB Error: Car \'", regoC1, "\' already exists.");
     }
   });
 
-  Car.findOne({'rego': 'ABC456'}, (error, car) => {
+  var regoC2 = 'ABC456';
+  Car.findOne({'rego': regoC2}, (error, car) => {
     if(!car) {
-      let carTwo = new Car();
-      (carTwo.rego = "ABC456"), (carTwo.make = "Mazda");
-      carTwo.model = "MX-5";
-      carTwo.colour = "blue";
-      carTwo.year = "2017";
-      carTwo.seats = "5";
-      carTwo.doors = "5";
-      carTwo.vehicleType = typeSports;
-      carTwo.location = locationOne;
+      let car2 = new Car();
+      (car2.rego = regoC2), (car2.make = "Mazda");
+      car2.model = "MX-5";
+      car2.colour = "blue";
+      car2.year = "2017";
+      car2.seats = "5";
+      car2.doors = "5";
+      car2.vehicleType = typeSports;
+      car2.location = location1;
     
       //Movement Block
       let movementsC2 = new Movement();
-      movementsC2.car = carTwo;
-      movementsC2.coordinates = locationOne.coordinates;
+      movementsC2.car = car2;
+      movementsC2.coordinates = location1.coordinates;
       movementsC2.save({}).catch();
     
-      carTwo.movements = movementsC2;
-      carTwo.save({}).catch(function(err) {
-        if (11000 === err.code || 11001 === err.code) {
-          console.log("DB Error: Duplicate of car:", carTwo.rego);
-        }
-      });
+      car2.movements = movementsC2;
+      car2.save({}).catch();
+
+    }
+    else
+    {
+      console.log("DB Error: Car \'", regoC2, "\' already exists.");
     }
   });
 
-  Car.findOne({'rego': 'ABC789'}, (error, car) => {
+  //Car 3 data Block
+  var regoC3 = 'ABC789';
+  Car.findOne({'rego': regoC3}, (error, car) => {
     if(!car) {
-      let carThree = new Car();
-      (carThree.rego = "ABC789"), (carThree.make = "Audi");
-      carThree.model = "A6";
-      carThree.colour = "red";
-      carThree.year = "2017";
-      carThree.seats = "5";
-      carThree.doors = "5";
-      carThree.vehicleType = typeLuxury;
-      carThree.location = locationTwo;
+      let car3 = new Car();
+      (car3.rego = regoC3), (car3.make = "Audi");
+      car3.model = "A6";
+      car3.colour = "red";
+      car3.year = "2017";
+      car3.seats = "5";
+      car3.doors = "5";
+      car3.vehicleType = typeLuxury;
+      car3.location = location2;
     
       //Movement Block
       let movementsC3 = new Movement();
-      movementsC3.car = carThree;
-      movementsC3.coordinates = locationTwo.coordinates;
+      movementsC3.car = car3;
+      movementsC3.coordinates = location2.coordinates;
       movementsC3.save({}).catch();
     
-      carThree.movements = movementsC3;
-      carThree.save({}).catch(function(err) {
-        if (11000 === err.code || 11001 === err.code) {
-          console.log("DB Error: Duplicate of car:", carThree.rego);
-        }
-      });      
+      car3.movements = movementsC3;
+      car3.save({}).catch();
+
+    }
+    else
+    {
+      console.log("DB Error: Car \'", regoC3, "\' already exists.");
     }
   });
 
-  Car.findOne({'rego': 'DEF125'}, (error, car) => {
+  //Car 4 data Block
+  var regoC4 = 'DEF125';
+  Car.findOne({'rego': regoC4}, (error, car) => {
     if(!car) {
-      let carFour = new Car();
-      (carFour.rego = "DEF125"), (carFour.make = "Mazda");
-      carFour.model = "CX-9";
-      carFour.colour = "silver";
-      carFour.year = "2017";
-      carFour.seats = "5";
-      carFour.doors = "5";
-      carFour.vehicleType = typeSuv;
-      carFour.location = locationThree;
+      let car4 = new Car();
+      (car4.rego = regoC4), (car4.make = "Mazda");
+      car4.model = "CX-9";
+      car4.colour = "silver";
+      car4.year = "2017";
+      car4.seats = "5";
+      car4.doors = "5";
+      car4.vehicleType = typeSuv;
+      car4.location = location3;
     
       //Movement Block
       let movementsC4 = new Movement();
-      movementsC4.car = carFour;
-      movementsC4.coordinates = locationThree.coordinates;
+      movementsC4.car = car4;
+      movementsC4.coordinates = location3.coordinates;
       movementsC4.save({}).catch();
     
-      carFour.movements = movementsC4;
-      carFour.save({}).catch(function(err) {
-        if (11000 === err.code || 11001 === err.code) {
-          console.log("DB Error: Duplicate of car:", carFour.rego);
-        }
-      });     
+      car4.movements = movementsC4;
+
+      car4.save({}).catch();
+    }
+    else
+    {
+      console.log("DB Error: Car \'", regoC4, "\' already exists.");
     }
   });
 
