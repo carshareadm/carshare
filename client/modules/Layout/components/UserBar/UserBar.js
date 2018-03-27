4c1ce18cd4d3591494425b6f97d45471e16f1b50
Inga Pflaumer <warda.elentari@gmail.com>
2018-03-27 21:27:21 +1100

    Login frontend (#31)
    * started working on frontend Form
    
    * Request and simple form validation
    
    * Form toggle fix

--- client/modules/Layout/components/UserBar/UserBar.js
+++ client/modules/Layout/components/UserBar/UserBar.js
@@ -8,10 +8,14 @@ export class UserBar extends Component{
 
 	constructor(props){
 		super(props);
-		this.state = {formIsVisible:false};
+		this.state = {
+			formIsVisible:false,
+			loggedIn:false,
+		};
 
 		//Bind the function to the class
 		this.formToggle = this.formToggle.bind(this);
+		this.executeLogin = this.executeLogin.bind(this);
 	}
 
 	formToggle(){
@@ -20,29 +24,71 @@ export class UserBar extends Component{
 		});
 	}
 
-	render(){
+	loggedInBar(){
 		return (
-		    <div className={styles.bar}>
+			<div className={styles.bar}>
 			    <h3>
-			    	<span className={styles.barItem} onClick={this.formToggle}>Login</span> 
 			    	<span className={styles.barItem}>	
-			    		&nbsp;or&nbsp;
-			    		<Link className={styles.barItem_link} to="/register">Register</Link>
+			    		Welcome to ShaCar
 			    	</span>
 			    </h3>
-			    <form style={{display:this.state.formIsVisible ? "block" : "none"}}>
-	                <label className={styles.labels} for="login">
-	                    <div className={styles.labelText}>Login</div>
-	                    <input type="text" name="login" id="login" />
-	                </label>
-	                <label className={styles.labels} for="password">
-	                    <div className={styles.labelText}>Password</div>
-	                    <input type="text" name="password" id="password"/>
-	                </label>
-	                <input type="submit" value="Submit" />
-            	</form>
+			</div>
+		);
+	}
+
+	executeLogin(event){
+		event.preventDefault();
+		const login = document.getElementById("login").value;
+		const pass = document.getElementById("password").value;
+
+		// Won't work in IE for now
+		fetch('/api/account/login', {
+			method: 'POST',
+			headers: {'content-type':'application/json'},
+			body: JSON.stringify({email: login, password: pass}),
+		})
+		.then((response) => {
+			if(!response.ok){
+				console.error("Something went wrong ",response);
+				return;
+			}
+			this.setState({
+				loggedIn: true,
+			});
+		})
+
+	}
+
+	loginFrm(){
+		return (
+			<div className={styles.bar}>
+			    <h3>
+			    	<span className={styles.barItem} onClick={this.formToggle}>Login</span>
+			    	{this.state.formIsVisible ? 
+			    		null :
+				    	<span className={styles.barItem}>	
+				    		&nbsp;or&nbsp;
+				    		<Link className={styles.barItem_link} to="/register">Register</Link>
+				    	</span>
+			    	}
+			    </h3>
+			    <form style={{display:this.state.formIsVisible ? "block" : "none"}} onSubmit={this.executeLogin}>
+		            <label className={styles.labels} htmlFor="login">
+		                <div className={styles.labelText}>Login</div>
+		                <input type="text" name="login" id="login" />
+		            </label>
+		            <label className={styles.labels} htmlFor="password">
+		                <div className={styles.labelText}>Password</div>
+		                <input type="text" name="password" id="password"/>
+		            </label>
+		            <input type="submit" value="Submit" />
+		    	</form>
 		    </div>
-  		);
+		);
+	}
+
+	render(){
+		return this.state.loggedIn ? this.loggedInBar() : this.loginFrm();
 	}
 
 }
