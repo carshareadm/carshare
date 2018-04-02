171077d67a8de144940a9c5cdb8a747f62eead9e
Inga Pflaumer <inga.pflaumer@gmail.com>
2018-04-02 15:27:34 +1000

    WIP

--- client/modules/Contact/Contact.js
+++ client/modules/Contact/Contact.js
@@ -9,39 +9,48 @@ export class Contact extends Component {
     // Here goes our page 
     return (
         <div className={styles.body}>
-            <h1 className={styles.title}>Contact Us</h1>
-            <p><strong>Contact Form</strong><br /></p>
-            <form>
-                <label className={styles.labels} for="firstName">
-                    <div className={styles.labelText}>First Name *</div>
-                    <input type="text" name="firstName" id="firstName" />
-                </label>
-                <label className={styles.labels} for="lastName">
-                    <div className={styles.labelText}>Last name *</div>
-                    <input type="text" name="lastName" id="lastName"/>
-                </label>
-                <label className={styles.labels} for="emailAddr">
-                    <div className={styles.labelText}>Email *</div>
-                    <input type="text" name="emailAddr" id="emailAddr"/>
-                </label>
-                <label className={styles.labels} for="messageText">
-                    <div className={styles.labelText}>Message</div>
-                    <input type="textArea" name="messageText" id="messageText"/>
-                </label>
-                <input type="submit" value="Submit" />
-            </form>
-            <p><strong>Need Help?</strong><br/>
-            Please contact us via the telephone numbers listed below or the contact form above.</p>
+            <div className="row no-gutters">
+                <div className="col-xs-12 col-md-6">
+                    <h1 className={styles.title}>Contact Us</h1>
+                    <p><strong>Need Help?</strong><br/>
+                    Please contact us via the telephone numbers listed below or the contact form above.</p>
 
-            <p><strong>Customer Services</strong><br />
-            Services available 7am - 7pm</p>
+                    <p><strong>Customer Services</strong><br />
+                    Services available 7am - 7pm</p>
 
-            <p className={styles.phoneNum}>1300 000 123</p>
+                    <p className={styles.phoneNum}>1300 000 123</p>
 
-            <p><strong>ShaCar Office Address:</strong><br />
-            1 Flinders St,<br />
-            Melbourne, Victoria</p>
-
+                    <p><strong>ShaCar Office Address:</strong><br />
+                    1 Flinders St,<br />
+                    Melbourne, Victoria</p>
+                </div>
+                <div className="col-xs-12 col-md-6">
+                    <p><strong>Contact Form</strong><br /></p>
+                    <form>
+                        <label className={styles.labels} htmlFor="title">
+                            <div className={styles.labelText}>Title *</div>
+                            <input type="text" name="firstName" id="title" placeholder="Please Enter"/>
+                        </label>
+                        <label className={styles.labels} htmlFor="firstName">
+                            <div className={styles.labelText}>First Name *</div>
+                            <input type="text" name="firstName" id="firstName" placeholder="Please Enter"/>
+                        </label>
+                        <label className={styles.labels} htmlFor="lastName">
+                            <div className={styles.labelText}>Last name *</div>
+                            <input type="text" name="lastName" id="lastName" placeholder="Please Enter"/>
+                        </label>
+                        <label className={styles.labels} htmlFor="emailAddr">
+                            <div className={styles.labelText}>Email *</div>
+                            <input type="text" name="emailAddr" id="emailAddr" placeholder="Please Enter"/>
+                        </label>
+                        <label className={styles.labels} htmlFor="messageText">
+                            <div className={styles.labelText}>Message</div>
+                            <input type="textArea" name="messageText" id="messageText" placeholder="Please Enter"/>
+                        </label>
+                        <input type="submit" value="Submit" />
+                    </form>
+                </div>
+            </div>
         </div>
     );
   }
