0208e9ea45e2ba51bdccb61bc4534fdca91fa367
Inga Pflaumer <warda.elentari@gmail.com>
2018-04-12 21:25:51 +1000

    Car time slots call implemented and tested (#73)

--- server/controllers/cars/cars.controller.js
+++ server/controllers/cars/cars.controller.js
@@ -1,6 +1,10 @@
 import Car from "../../models/car";
 import VehicleType from "../../models/vehicleType";
 import Location from "../../models/location";
+import Booking from "../../models/booking";
+
+import mongoose from 'mongoose';
+import moment from 'moment';
 
 
 const getCars = function(req, res) {
@@ -19,13 +23,53 @@ const getCars = function(req, res) {
     });
 };
 
+const parseBookings = function(booking, pStart, pEnd){
+  const start = moment.max(moment(booking.startsAt), pStart);
+  const end = moment.min(moment(booking.endsAt), pEnd);
+  const hours = end.diff(start, 'hours');
+  const times = [];
+  // We add 1 hour to the booking length to make sure the car will be returned on time.
+  for(let i=0; i<=hours; i++){
+    times.push({
+        hour: start.format("HH:mm"),
+        weekday: start.format("ddd"),
+      });
+    start.add(1, 'h');
+  }
+
+  return times;
+}
+
 const getTimes = function(req, res) {
-  console.log(req.params.carId);
-  // Find car by ID and then times available from it -> Car.find({'_id': })
+  // To allow for weeklly display and switch, we take Start period and End period from URL
+  const startsAt = moment(req.query.start);
+  const endsAt =  moment(req.query.end);
+  //Book ID also comes from the URL
+  Booking.find({
+      startsAt:{$lt:endsAt.toDate()}, 
+      endsAt:{$gt:startsAt.toDate()}, 
+      car:mongoose.Types.ObjectId(req.params.carId)
+    }).exec((err, bookings)=> {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        let times = [];
+        // For each found booking we count the hours included and add them to array we return
+        bookings.forEach(b => {
+          times = times.concat(parseBookings(b, startsAt, endsAt));
+        });
+        res.status(200).send(times);
+      }
+    });
+};
+
+const getCar = function(req, res) {
+  // WIP 
   res.status(200).send([]);
 }
 
 module.exports = {
   getCars: getCars,
   getTimes: getTimes,
+  getCar: getCar,
 };
