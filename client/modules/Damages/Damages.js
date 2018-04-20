d76610964d10470a07f6b1a2c72c00630d3452ef
Inga Pflaumer <inga.pflaumer@gmail.com>
2018-04-20 22:37:29 +1000

    Frontend - no Image Upload yet

--- client/modules/Damages/Damages.js
+++ client/modules/Damages/Damages.js
@@ -6,6 +6,8 @@ import {
 	FormGroup,
 	Label,
 	Input,
+	Form,
+	FormFeedback,
 	FormText,
 	Dropdown,
 	DropdownToggle,
@@ -27,53 +29,98 @@ import stylesMain from '../../main.css'
 
 export class Damages extends Component {
 
-	constructor(props) {
+	constructor(props){
 	    super(props);
 	    this.state = {
-	      description: "",
+	      descr: "",
 	      image: "",
-	      booking:{}
 	    };
   	}
 
 	valids = {};
 
-  validate(){
-    const valids = {
-      description: this.state.message.length > 0,
-    }
-    return valids;
-  }
+  	validate(){
+	    const valids = {
+	      descr: this.state.descr.length > 0,
+	    }
+	    return valids;
+	  }
 
-  handleSubmit(event) {
-    event.preventDefault();
+   	isValid(key){
+	    const valid = this.valids[key];
+	    return valid;
+  	}
 
-    if (this.formIsValid()) {
-      http
-        .client()
-        //How to get booking ID from url to give it to the controller?
-        .post(`/${booking}/createDamage`, {
-          description:this.state.description
-        })
-	    .then(res => {
-	        console.log(res);
-	        this.setState({ successAlertOpen: true });
-	    })
-	    .catch(err => {
-	        console.log(err);
-	        this.setState({ failAlertOpen: true });
-	    })
-    }
-  }
+  	formIsValid(){
+  		return Object.keys(this.valids).every(field => this.valids[field] === true);
+  	}
+
+  	handleSubmit(event){
+	    event.preventDefault();
+	    if (this.formIsValid()) {
+	      http
+	        .client()
+	        .post(`/${booking}/createDamage`, {
+	          description:this.state.descr,
+	          booking:this.props.query.bookingId
+	        })
+		    .then(res => {
+		        console.log(res);
+		        this.setState({ successAlertOpen: true });
+		    })
+		    .catch(err => {
+		        console.log(err);
+		        this.setState({ failAlertOpen: true });
+		    })
+	    }
+  	}
+
+	handleInputChange(event){
+	  	this.setState({
+	  		descr:event.target.value
+	  	});
+	}
+
+  	handleImageUploaded(){
+
+  	}
 
 	render() {
+		this.valids = this.validate();
+		const isDisabled = !this.formIsValid() || this.state.successAlertOpen || this.state.failAlertOpen;
 		return (
 			<div className={stylesMain.body}>
 				<Container>
 					<Row>
-					<h4>Damage Report</h4>
-					<Loading msg={this.state.loadingMsg}></Loading>
-					<FileUploader onFileUploaded={this.handleImageUploaded.bind(this)}></FileUploader>
+						<h4>Damage Report</h4>
+						<FileUploader onFileUploaded={this.handleImageUploaded.bind(this)}></FileUploader>
+						<Form className="novalidate" onSubmit={this.handleSubmit.bind(this)}>
+				            <FormGroup>
+				                <Label htmlFor="descr">Description *</Label>
+				                <Input
+				                  type="text"
+				                  name="descr"
+				                  id="descr"
+				                  placeholder="Describe the damage"
+				                  className={this.isValid('descr') ? '' : 'is-invalid'}
+				                  value={this.state.description}
+				                  onChange={this.handleInputChange.bind(this)}
+				                />
+				                <FormFeedback>
+				                  A  description address is required.
+				                </FormFeedback>
+				            </FormGroup>
+			                 <Button
+				                type="submit"
+				                disabled={isDisabled}
+				                outline
+				                color="success"
+				                size="lg"
+				                block
+				              >
+				                Submit
+				              </Button>
+			            </Form>
 					</Row>
 				</Container>
 			</div>
