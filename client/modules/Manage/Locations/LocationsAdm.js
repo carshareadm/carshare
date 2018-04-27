625bd0c82ab41d633e3bfdfef00edc21e7787a2c
Paul Crow <paulcrow@Korriban.local>
2018-04-27 23:05:13 +0800

    manage locations backend for update, plus tests
    locations adm list and edit component

--- client/modules/Manage/Locations/LocationsAdm.js
+++ client/modules/Manage/Locations/LocationsAdm.js
@@ -9,23 +9,80 @@ import {
   Label,
   Form,
 } from "reactstrap";
+import {Typeahead} from 'react-bootstrap-typeahead';
+import LocationAdm from './components/LocationAdm';
 
-import * as http from "../../../util/http";
+import manageSvc from "../../../services/manage.service";
 import stylesMain from '../../../main.css';
 
 export class LocationsAdm extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      locations: [],
+      selectedLocation: null,
+    };
+  }
+
+  componentDidMount() {
+    this.getLocations();
+  }
+
+  getLocations() {
+    manageSvc.locations.getAll()
+      .then(res => this.setState({locations: res.data}))
+      .catch(e => console.log(e));
+  }
+
+  handleLocationSelected(evt) {
+    // evt is an array of objects
+    this.setState({selectedLocation: evt[0]});
+  }
+
+  handleSaved(saveLocation) {
+    this.getLocations();
+  }
+
+  selectedLocationForm() {
+    return (
+      !this.state.selectedLocation 
+      ? '' 
+      : <LocationAdm
+          onSaved={this.handleSaved.bind(this)}
+          location={this.state.selectedLocation} />
+    );
+  }
+
+  clearSearch() {
+    this.refs.typeahead.getInstance().clear();
+    this.setState({selectedLocation: null});
   }
 
   render() {
     return (
       <div className={stylesMain.body}>
-        <div className="row">
+        <Row>
           <Col>
             <h1 className={stylesMain.title}>Manage Locations</h1>
+            <div className="input-group">
+              <Typeahead
+                ref="typeahead"
+                align="left"
+                placeholder="Search for location..."
+                onChange={(e) => this.handleLocationSelected(e)}
+                labelKey={option => `${option.name}`}
+                options={this.state.locations}
+                filterBy={['name']}
+              />              
+              <div className="input-group-append">
+                <Button className="btn btn-outline-secondary" type="button" onClick={(e) => this.clearSearch()}>Clear</Button>
+              </div>
+            </div>
+            <hr />
           </Col>
-        </div>
+
+        </Row>
+        {this.selectedLocationForm()}
       </div>
     );
   }
