1a159259623d9c143907f4e523a89b58d59b6ed7
Inga Pflaumer <warda.elentari@gmail.com>
2018-05-08 00:34:27 +1000

    Search on FAQ not styled (#156)

--- client/modules/Faq/components/Search.js
+++ client/modules/Faq/components/Search.js
@@ -3,61 +3,26 @@ import React, { Component, PropTypes } from "react";
 // Import Style
 import stylesMain from '../../../main.css';
 
-function setupGoogleSearch(callback) {
-    window.__gcse = {
-      parsetags: 'explicit',
-      callback: callback,
-    };
-
-    (function() {
-      var cx = '000397261121787994763:drhydcsx3mk';
-      var gcse = document.createElement('script');
-      gcse.type = 'text/javascript';
-      gcse.async = true;
-      gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
-      var s = document.getElementsByTagName('script')[0];
-      s.parentNode.insertBefore(gcse, s);
-    })();
-}
-
 class Search extends Component {
-
-  renderGoogleSearch() {
-    google.search.cse.element.render({
-      div: "searchbox-content",
-      tag: 'searchbox-only',
-      attributes: { resultsUrl: '/search' }
-    });
-
-    const results = document.getElementById("gcse-results-wrapper");
-    if (results) { results.innerHTML = ""; }
-    google.search.cse.element.render({
-      div: "gcse-results-wrapper",
-      tag: 'searchresults-only'
-    });
+  constructor(){
+    super()
+    this.saveQuery = this.saveQuery.bind(this);
+    this.clear = this.clear.bind(this);
   }
 
-  googleSearchCallback() {
-    if (document.readyState == 'complete') {
-          this.renderGoogleSearch();
-     } else {
-           google.setOnLoadCallback(() => this.renderGoogleSearch(), true);
-      }   
+  clear() {
+    this.props.onSearch("");
   }
 
-  componentDidMount() {
-    setupGoogleSearch(() => this.googleSearchCallback());
-	}
+  saveQuery(event) {
+    this.props.onSearch(event.target.value)
+  }
 
   render() {
     return(
-      <div id="searchbox-content" style={{ width: '90%' }} className="content">
-        <div className="gcse-search" 
-            data-resultsWrapperClass="gcse-results-wrapper"
-            data-resultsUrl="http://www.google.com" 
-            data-newWindow="true" 
-            data-queryParameterName="search" >
-        </div>
+      <div style={{ width: '90%' }} className="content">
+        <input name="searchquery" onChange={this.saveQuery} value={this.props.query} />
+        <button onClick={this.clear}>clear</button>
       </div>
     );
   }
